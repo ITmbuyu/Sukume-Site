82be7e061f3f32b7518b663b9be6e37dd59722b1
ITmbuyu <41137033+ITmbuyu@users.noreply.github.com>
2023-10-11 11:47:43 +0200

    changes

---
+++ assets/css/why-us-table.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Public Speaking Events</title>
+	<link type="text/css" rel="Stylesheet" href="style.css" />
+</head>
+<body>
+	<table class="talks-table">
+		<tr>
+			<th>Client</th>
+			<th>Nature of Work</th>
+		</tr>
+		<tr>
+			<td>Department of Trade &amp; Industry (South Africa)</td>
+			<td>Marketing and Communication Research Review</td>
+		</tr>
+		<tr>
+			<td>Botswana Trade &amp; Investment  Centre (Botswana)</td>
+			<td>Brand Botswana Strategy Development and Advisory</td>
+		</tr>
+		<tr>
+			<td>African Union Commission (Ethiopia)</td>
+			<td>Resource Mobilisation Strategy Review and Implementation</td>
+		</tr>
+		<tr>
+			<td>Vuka Africa Foundation (South Africa)</td>
+			<td>Trade Missions (InterAfrica Regional Trade and Integration)</td>
+		</tr>
+		<tr>
+			<td>Africa-Israel Chamber of Commerce (South Africa)</td>
+			<td>Trade and Investment Promotion and Facilitation</td>
+		</tr>
+	</table>
+</body>
+</html>
